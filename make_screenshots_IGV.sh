c720423fa8beaf9d26ac1663e94f218c6d9b0e68
reykajayasinghe <reykajayasinghe@gmail.com>
2016-03-04 13:26:23 -0600

    minor edits for complex indel input data

--- make_screenshots_IGV.sh
+++ make_screenshots_IGV.sh
@@ -1,9 +1,10 @@
 if [ $# -eq 0 ];then
 
 	echo "No arguments supplied. This script will make a .bat file to run in IGV to make many screenshots given a MAF."
-	echo "bash make_igv_screenshots.sh <INPUT> <ANNO>"
-echo "bash scripts/make_screenshots_complex.sh /gscuser/rjayasin/projects/Complex_full/complex_indel/data/PCGP/sfoltz_complex/test_folder2/example_info /gscuser/rjayasin/projects/Complex_full/complex_indel/data/PCGP/sfoltz_complex/test_folder2/origdata/TCGA-E2-A105.somatic_filtered_spaced"	
-echo "After script finishes running open up .bat in IGV to start screenshots. (.bat file located in location of MAF file and under directory MAF_snaps)"
+	echo "bash make_screenshots_IGV.sh <INPUT> <ANNO>"
+echo "After script finishes running open up .bat in IGV to start screenshots. (.bat file located in location of input ANNO file and under directory ANNO_snaps)"
+	echo "INPUT: CANCER_TYPE SAMPLE TUMORSAMPLE TUMOR.bam NORMALSAMPLE NORMAL.bam"
+	echo "ANNO: CHR START INSERTION DELETION INSERTEDSEQUENCE NORMALSAMPLE TUMORSAMPLE"
 	exit 1
 fi
 
@@ -14,33 +15,29 @@ path=`readlink -f ${2} | awk '{print $0"_snaps"}'` #Create new directory with na
 mkdir ${path}
 
 > ${path}/${file}.bat
-#samples=`cut -f 1 ${1}|uniq | grep -v "^$" ` #Get all unique samples
-samples=`cut -f 2 ${2}|awk '{print $1}'|uniq | grep -v "^$" ` #Get all unique samples
+samples=`cut -f 6 ${2}|awk '{print $1}'|uniq | grep -v "^$" ` #Get all unique samples
 echo "genome hg19" >> ${path}/${file}.bat
 while read sample
 do
 echo $sample
 ##Identify primary and germline samples
-	bam_path_primary=`fgrep ${sample} ${1}|cut -f 3`
-	bam_path_germline=`fgrep ${sample} ${1}|cut -f 5`
+	bam_path_primary=`fgrep ${sample} ${1}|cut -f 4`
+	bam_path_germline=`fgrep ${sample} ${1}|cut -f 6`
 	echo "new" >> ${path}/${file}.bat
 	echo "load ${bam_path_primary}" >> ${path}/${file}.bat
 	echo "load ${bam_path_germline}" >> ${path}/${file}.bat
 	echo "snapshotDirectory ${path}" >> ${path}/${file}.bat
 
-	#mutations=`fgrep ${sample} ${2} |awk -F '\t' '{print $8"_"$10"_"$11"_"$3"_"$5}'`
-	###EXAMPLE:GRIK2_H_LC-SJETV028_6_102322609_102322612_4
-	#mutations=`fgrep ${sample} ${2} |awk -F'[:\t| ]' '{print $1"_"$2"_"$4"_"$5}' `
-	mutations=`fgrep -w ${sample} ${2}|awk 'BEGIN{FS="[:,\t,|,=,;, ]"}{print $1"_"$2"_"$4"_"$5"_"$12"_"$20}' `
+	###EXAMPLE:CHR_START_INSERTION_DELETION_INSERTEDSEQ_NORMALSAMPLE
+	mutations=`fgrep -w ${sample} ${2}|awk 'BEGIN{FS="[:,\t,|,=,;, ]"}{print $1"_"$2"_"$3"_"$4"_"$5"_"$6}' `
 	while read mutation
 	do
 	echo $mutation
-		start=`echo "$mutation"|awk '{split($1,a,"_"); print a[5]}'`
-		start_new=`expr ${start} - 5`
-		stop=`echo "$mutation"|awk '{split($1,a,"_"); print a[5]}'`
-		stop_new=`expr ${stop} + 10`	
-		chr=`echo "$mutation"|awk '{split($1,a,"_"); print a[4]}'`
-	#	position=`echo "$mutation"|awk '{split($1,a,"_"); print a[4]":"a[5]"-"a[6]}'`
+		start=`echo "$mutation"|awk '{split($1,a,"_"); print a[1]}'`
+		start_new=`expr ${start} - 20`
+		stop=`echo "$mutation"|awk '{split($1,a,"_"); print a[1]}'`
+		stop_new=`expr ${stop} + 100`	
+		chr=`echo "$mutation"|awk '{split($1,a,"_"); print a[0]}'`
 	
 		echo "goto ${chr}:${start}" >> ${path}/${file}.bat
 		echo "sort base" >> ${path}/${file}.bat
